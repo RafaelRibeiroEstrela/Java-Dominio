b1fc30adce72de20d1d16db0306d2eefc8185755
RafaelRibeiroEstrela <74848416+RafaelRibeiroEstrela@users.noreply.github.com>
2022-03-31 06:52:40 -0400

    update

---
+++ api-dominio/src/test/java/com/example/excessoes/ApiDominioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.excessoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
