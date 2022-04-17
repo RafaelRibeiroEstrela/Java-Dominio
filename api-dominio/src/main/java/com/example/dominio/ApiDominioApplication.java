9262db5b73b552940ad449b78c7a99f336b7ed94
RafaelRibeiroEstrela <74848416+RafaelRibeiroEstrela@users.noreply.github.com>
2022-04-17 13:31:30 -0400

    update

---
+++ api-dominio/src/main/java/com/example/dominio/ApiDominioApplication.java
@@ -0,0 +1,13 @@
+package com.example.dominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDominioApplication.class, args);
+	}
+
+}
---
+++ api-dominio/src/test/java/com/example/dominio/ApiDominioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
