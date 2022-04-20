8e971a8dfbc8b189f7ea10927cd19ada88c96e02
Rafael Estrela <rafael.r.estrela@gmail.com>
2022-04-19 22:00:05 -0400

    update

--- api-dominio/src/main/java/com/example/dominio/models/Cargo.java
+++ api-dominio/src/main/java/com/example/dominio/models/Cargo.java
@@ -1,5 +1,91 @@
 package com.example.dominio.models;
 
-public class Cargo {
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "DOMINIO.TB_CARGO")
+public class Cargo implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_CARGO")
+	@SequenceGenerator(name = "SEQ_CARGO", sequenceName = "DOMINIO.SEQ_CARGO", allocationSize = 1)
+	@Column(name = "ID_CARGO")
+	private Long id;
+	
+	@Column(name = "NOME_CARGO")
+	private String nome;
+	
+	@Column(name = "SALARIO")
+	private Double salario;
+	
+	public Cargo() {
+		
+	}
+
+	public Cargo(Long id, String nome, Double salario) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.salario = salario;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cargo other = (Cargo) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Cargo [id=" + id + ", nome=" + nome + ", salario=" + salario + "]";
+	}
+	
+	
 
 }
--- api-dominio/src/main/java/com/example/dominio/models/Departamento.java
+++ api-dominio/src/main/java/com/example/dominio/models/Departamento.java
@@ -1,5 +1,95 @@
 package com.example.dominio.models;
 
-public class Departamento {
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "DOMINIO.TB_DEPARTAMENTO")
+public class Departamento implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_DEPARTAMENTO")
+	@SequenceGenerator(name = "SEQ_DEPARTAMENTO", sequenceName = "DOMINIO.SEQ_DEPARTAMENTO", allocationSize = 1)
+	@Column(name = "ID_DEPARTAMENTO")
+	private Long id;
+	
+	@Column(name = "NOME_DEPARTAMENTO")
+	private String nome;
+	
+	@ManyToOne
+	@JoinColumn(name = "ID_EMPRESA")
+	private Empresa empresa;
+	
+	public Departamento() {
+		
+	}
+
+	public Departamento(Long id, String nome, Empresa empresa) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.empresa = empresa;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Departamento other = (Departamento) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Departamento [id=" + id + ", nome=" + nome + ", empresa=" + empresa + "]";
+	}
+	
+	
 
 }
